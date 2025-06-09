fed2852d1473b8e0d3338336f42dfecbba80d7b5
Serkan BULUT <serkan@bulut.gen.tr>
2025-06-10 02:07:28 +0300

    4

---
+++ src/BulutBusiness.Core/Persistence/ServiceCollectionServicesAddExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace BulutBusiness.Core.Persistence;
+
+public static class ServiceCollectionServicesAddExtensions
+{
+    public static IServiceCollection AddApplicationPersistenceServices(
+        this IServiceCollection services,
+        Assembly applicationAssembly,
+        Assembly persistenceAssembly)
+    {
+        // Application katmanındaki tüm arayüzleri bul
+        var interfaceTypes = applicationAssembly.GetTypes()
+            .Where(t => t.IsInterface && t.Name.StartsWith("I") && t.Name.EndsWith("Service"));
+
+        // Persistence katmanındaki tüm implementasyonları bul
+        var implementationTypes = persistenceAssembly.GetTypes()
+            .Where(t => t.IsClass && !t.IsAbstract && t.Name.EndsWith("Service"));
+
+        foreach (var interfaceType in interfaceTypes)
+        {
+            // Persistence katmanındaki implementasyonları arayüzlere eşleştir
+            var implementationType = implementationTypes
+                .FirstOrDefault(t => $"I{t.Name}" == interfaceType.Name);
+
+            if (implementationType != null)
+            {
+                // Scoped yaşam süresiyle DI'a ekle
+                services.AddScoped(interfaceType, implementationType);
+            }
+        }
+        return services;
+    }
+}
